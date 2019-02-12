5da1b5c426be5d26e0721b67e6fedb009e389793
Darryl <darryltanggy@gmail.com>
2019-02-12 19:09:44 +0800

    login page

--- controllers/index.js
+++ controllers/index.js
@@ -15,4 +15,8 @@ router.get('/register', function(req, res, next) {
   res.render('signup', { title: 'Register' });
 });
 
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Log In' });
+});
+
 module.exports = router;
