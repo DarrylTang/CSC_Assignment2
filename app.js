75ef743bcbac34900f2e5232bdde36657c5e6a9e
Bryan Fu <bryanfwh@gmail.com>
2019-02-12 15:18:47 +0800

    -

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 
 app.set('view engine', 'hbs');
+
 // app.engine('html', require('hbs').renderFile);
 // app.set('view engine', 'html');
 
