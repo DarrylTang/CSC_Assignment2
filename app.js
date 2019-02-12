e215b38079560936cb0327d2966b52017fcdcf77
mailmancow <halo56060@gmail.com>
2019-02-12 14:50:38 +0800

    uploading image to s3

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./controllers/index');
 var mailerRouter = require('./controllers/mailer');
+var uploadRouter = require('./controllers/image-upload');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/', mailerRouter);
+app.use('/', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ controllers/image-upload.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+var app = express();
+const upload = require('../Services/image-upload');
+
+const singleUpload = upload.single('image')
+
+app.post('/image-upload', function(req, res) {
+  singleUpload(req, res, function(err, some) {
+    if (err) {
+      return res.status(422).send({errors: [{title: 'Image Upload Error', detail: err.message}] });
+    }
+
+    return res.json({'imageUrl': req.file.location});
+  });
+})
+
+module.exports = app;
