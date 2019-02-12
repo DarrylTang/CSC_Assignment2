4c219b29c75255ebcf08ec882164312e0c76eed1
Bryan Fu <bryanfwh@gmail.com>
2019-02-12 16:55:53 +0800

    Fixed captcha

--- controllers/createAccount.js
+++ controllers/createAccount.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var request = require('request');
 var jsonParser = bodyParser.json({ extended: false});
 
 
@@ -14,24 +15,26 @@ app.post('/register', jsonParser, function (req, res) {
         var password = req.body.password;
         var recaptcha = req.body.recaptcha;
         var hostName = req.body.hostName;
-        console.log("HI" + recaptcha);
+        console.log(recaptcha + "HI");
         
-        if(recaptcha != null || recaptcha != undefined || recaptcha != '') {
+        if(recaptcha == null || recaptcha == undefined || recaptcha == '') {
+            res.send({ "success":false, "errorMsg" : "Please complete captcha" });
+        }
+        else {
             var secretKey = "6Le40ZAUAAAAALyy3BgGkhQe08NfvKTwGzweEaO0";
             var verificationUrl = "https://www.google.com/recaptcha/api/siteverify?secret=" + secretKey
                 + "&response=" + recaptcha + "&remoteip=" + req.connection.remoteAddress;
+            
             request(verificationUrl, function (error, response, body) {
                 body = JSON.parse(body);
+                
                 if (body.success !== undefined && !body.success) {
-                    res.send({ "errorMsg": "Failed captcha verification" });
+                    res.send({"success":false, "errorMsg": "Failed captcha verification" });
                 }
                 else {
-                    res.send({success: true});
+                    res.send({"success": true});
                 }
             });
-        }
-        else {
-            res.send({ "success":false, "errorMsg" : "Please select captcha" });
         }
     });
 
